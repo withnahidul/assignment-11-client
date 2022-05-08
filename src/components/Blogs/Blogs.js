bd804acf7c5adfd72b4c92b21f3ca3a0fa4b13c3
withnahidul <nahidulislam6800@gmail.com>
2022-05-08 11:01:34 +0600

    qustion-n-2

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -48,37 +48,80 @@ const Blogs = () => {
       </div>
       <div className="shadow-xl rounded-lg border border-sky-800 px-10 py-5 my-5">
         <h2 className=" text-xl font-mono font-bold">
-          2.Why are you using{" "}
-          <span className="bg-slate-200 px-1 rounded">firebase</span>? What
-          other options do you have to implement authentication?
+          2.When should you use{" "}
+          <span className="bg-slate-200 px-1 rounded">Nodejs</span>and When you
+          should you use Mongodb?
         </h2>
 
         <ul className="text-left list-decimal">
           <p className="my-4">There are 3 main services I like:</p>
           <li>
-            <span className="font-bold">Apps development:</span>
+            <span className="font-bold">use nodejs:</span>
+            <p>
+              Since we have outlined the advantages and disadvantages of
+              Node.js, let's see how they translate into the practical aspects.
+              We will try to determine when to use Node.js and what is Node.js
+              used for in web development in order to achieve the best
+              application performance.
+            </p>
             <ul className="text-left list-disc list-inside">
-              <li>Firebase web</li>
-              <li>Firebase Auth</li>
-              <li>Cloud storage</li>
-              <li>Realtime Database</li>
+              <li>Real-time applications</li>
+              <li>IoT applications</li>
+              <li>Collaborative tools.</li>
+              <li>Data streaming applications.</li>
+              <li>Applications relying on scalability.</li>
             </ul>
+            <p>
+              Node.js is not good: While there are quite a number of use cases
+              that Node.js fits perfectly, there are situations when you should
+              look for other platforms, as Node.js will not provide the best
+              performance.
+              <ul className="text-left list-disc list-inside">
+                <li>Applications with heavy computing server-side</li>
+                <li>CRUD applications.</li>
+                <li>Server-side web applications with relational databases.</li>
+              </ul>
+            </p>
           </li>
           <li>
-            <span className="font-bold">Quality Improvement:</span>
+            <span className="font-bold">Mongo DB:</span>
             <ul className="text-left list-disc list-inside">
-              <li>Firebase crashlytics</li>
-              <li>Performance Monitoring Tools</li>
-              <li>Firebase test lab</li>
+              <p>
+                Since, MongoDB is a NoSQL database, so we need to understand
+                when and why we need to use this type of database in the
+                real-life applications. Since in normal circumstances, MongoDB
+                always preferred by the developers or project managers when our
+                main concern is the deal with large volume of data with a high
+                performance. If we want to insert thousands of records in a
+                second, then MongoDB is the best choice for that. Also,
+                horizontal scaling (adding new columns) is not so easy process
+                in any RDBMS systems. But in case of MongoDB, it is very much
+                easy since it is a schema less database. Also, this type of work
+                can be directly handled by the application automatically. There
+                is no need to any type of administrative work for perform any
+                type of horizontal scaling in the MongoDB. MongoDB is good for
+                the below types of situations :
+              </p>
+              <li>E-Commerce type of product-based applications</li>
+              <li>Blog and Content Management systems</li>
+              <li>High Speed logging, caching etc in the Real time</li>
+              <li>Need to maintain location wise Geospatial data</li>
+              <li>
+                For maintains data related to the Social and Networking types
+              </li>
+              <li>
+                If application is a loosely coupled mechanism means design may
+                change at any point of time.
+              </li>
             </ul>
+            <p>
+              MongoDB is not only database which can perform only CRUD (Create,
+              Read, Update and Delete) operations with the data. Except these,
+              MongoDB also contains so many important features due which MongoDB
+              become the most popular database in the NoSQL category
+            </p>
           </li>
-          <li>
-            <span className="font-bold">Instrument for Business group:</span>
-            <ul className="text-left list-disc list-inside">
-              <li>Firebase in-app messaging</li>
-              <li>Firebase A/B Testing</li>
-            </ul>
-          </li>
+
         </ul>
       </div>
       <div className="shadow-xl rounded-lg border border-sky-800 px-10 py-5 my-5">
